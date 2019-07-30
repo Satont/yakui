6c0287bac2cbac9a165b89532cb9e92877b1a0bc
Satont <satontworldwide@gmail.com>
2019-07-30 15:45:18 +0300

    feat(users): count watched time of users

--- systems/users.js
+++ systems/users.js
@@ -1,10 +1,13 @@
 const { io } = require("../libs/panel")
+const axios = require('axios')
 
 class Users {
   constructor() {
     this.settings
     this.start()
     this.sockets()
+    this.onlineUsers = []
+    this.checkInterval = setInterval(() => this.checkOnline(), 5 * 1000)
   }
   async start() {
     this.settings = (await global.db.select('*').from('settings').where('system', 'users'))[0].data
@@ -12,6 +15,16 @@ class Users {
   async parse(msg) {
     await global.db('users').insert({ id: Number(msg.tags.userId), username: msg.username }).then(() => {}).catch(() => {})
     await global.db('users').where({ id: Number(msg.tags.userId) }).increment({ messages: 1, points: this.settings.pointsPerMessage }).update({username: msg.username})
+  }
+  async checkOnline() {
+    //if (!global.twitch.uptime || !this.settings.enabled) return
+    let request = await axios.get(`http://tmi.twitch.tv/group/user/${process.env.TWITCH_CHANNEL.toLowerCase()}/chatters`)
+    let response = await request.data
+    let now = []
+    for (let key of Object.keys(response.chatters)) {
+      if (Array.isArray(response.chatters[key])) now = now.concat(response.chatters[key])
+    }
+    console.log(now)
   }
   async sockets() {
     let self = this
