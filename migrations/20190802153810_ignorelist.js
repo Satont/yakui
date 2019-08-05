5839758dc5e106fa3412d44bb49867f615f9ce65
Satont <satontworldwide@gmail.com>
2019-08-05 17:14:49 +0300

    fix(chore): fix initial setup

--- migrations/20190802153810_ignorelist.js
+++ migrations/20190802153810_ignorelist.js
@@ -3,6 +3,7 @@ exports.up = function(knex) {
   return Promise.all([
     knex('settings').select().where('system', 'users').then((rows) => {
      let actualData = rows[0]
+     if (!actualData) return
      actualData.data.ignorelist = []
       return knex('settings').where('system', 'users').update({ data: actualData.data })
     })
