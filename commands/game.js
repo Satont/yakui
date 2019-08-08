f05eac72df02446dfc2b9653810c6d353e134811
Satont <satontworldwide@gmail.com>
2019-08-08 20:21:55 +0300

    feat(title,game): add check for permission, send current stream data

--- commands/game.js
+++ commands/game.js
@@ -1,11 +1,18 @@
 const { getSuggestedGame, setGame } = require('../systems/twitch')
 const { say } = require('../systems/customCommands')
+const permissions = require('../libs/permissions')
 
 module.exports = {
   name: 'game',
   visible: false,
+  permission: 'moderator',
   async run(command, message, userstate) {
     message = message.split(' '); message.shift(); message = message.join(' ')
+
+    if (!permissions.hasPerm(userstate.badges, this.permission) || !message.length) {
+      return say(`${userstate.username} ${global.tmi.streamData.game}`)
+    }
+
     try {
       let find = await getSuggestedGame(message)
       await setGame(find)
