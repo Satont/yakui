dcdcd20eeca76d4db1ae002acf4e26962b4a43be
Satont <satontworldwide@gmail.com>
2019-09-05 01:37:20 +0300

    feat(events): add javascript filter
    Closes #73

--- systems/events.js
+++ systems/events.js
@@ -1,6 +1,7 @@
 const { io } = require('../libs/panel')
 const { say } = require('./customCommands')
 const _ = require('lodash')
+const safeEval = require('safe-eval')
 
 class Events {
   events = []
@@ -14,27 +15,52 @@ class Events {
     let event = this.events.find(o => o.name === what)
     event.operations = _.isPlainObject(event.operations ) ? [] : event.operations
     for (let operation of event.operations) {
+      if (operation.filter) {
+        if (!await this.filter(operation.filter, opts)) break;
+      }
       if (operation.key === 'sendMessage') {
         let message = operation.message
-        message = message.replace('$username', opts.username || '')
-        message = message.replace('$amount', opts.amount || '')
-        message = message.replace('$currency', opts.currency || '')
-        message = message.replace('$message', opts.message || '')
-        message = message.replace('$subTier', opts.subTier === 'Twitch Prime' ? 'Twitch Prime' : opts.subTier / 1000)
-        message = message.replace('$subStreak', opts.subStreak || '')
-        message = message.replace('$subGiftRecipient', opts.subGiftRecipient || '')
-        message = message.replace('$subGifterCount', opts.subGifterCount || '')
-        message = message.replace('$emoteOnlyState', opts.emoteOnlyState ? 'enabled' : 'disabled')
-        message = message.replace('$hostedViewers', opts.hostedViewers || '')
-        message = message.replace('$hostingViewers', opts.hostingViewers || '')
-        message = message.replace('$raidViewers', opts.raidViewers || '')
-        message = message.replace('$slowModeState', opts.slowModeState ? 'enabled' : 'disabled')
-        message = message.replace('$slowModeLength', opts.slowModeLength || '')
-        message = message.replace('$subsOnlyChatState', opts.subsOnlyChatState ? 'enabled' : 'disabled')
+        .replace('$username', opts.username || '')
+        .replace('$amount', opts.amount || '')
+        .replace('$currency', opts.currency || '')
+        .replace('$message', opts.message || '')
+        .replace('$subTier', opts.subTier === 'Twitch Prime' ? 'Twitch Prime' : opts.subTier / 1000)
+        .replace('$subStreak', opts.subStreak || '')
+        .replace('$subGiftRecipient', opts.subGiftRecipient || '')
+        .replace('$subGifterCount', opts.subGifterCount || '')
+        .replace('$emoteOnlyState', opts.emoteOnlyState ? 'enabled' : 'disabled')
+        .replace('$hostedViewers', opts.hostedViewers || '')
+        .replace('$hostingViewers', opts.hostingViewers || '')
+        .replace('$raidViewers', opts.raidViewers || '')
+        .replace('$slowModeState', opts.slowModeState ? 'enabled' : 'disabled')
+        .replace('$slowModeLength', opts.slowModeLength || '')
+        .replace('$subsOnlyChatState', opts.subsOnlyChatState ? 'enabled' : 'disabled')
 
         await say(message)
       }
     }
+  }
+  async filter (filter, opts) {
+    const toEval = `(async function evaluation () {  return ${filter} })()`
+    const context = {
+      $username: _.get(opts, 'username', null),
+      $amount: _.get(opts, 'amount', null),
+      $message: _.get(opts, 'message', null),
+      $subTier: _.get(opts, 'subTier', null),
+      $subStreak: _.get(opts, 'subStreak', null),
+      $subGiftRecipient: _.get(opts, 'subGiftRecipient', null),
+      $subGifterCount: _.get(opts, 'subGifterCount', null),
+      $emoteOnlyState: _.get(opts, 'emoteOnlyState', null),
+      $hostedViewers: _.get(opts, 'hostedViewers', null),
+      $hostingViewers: _.get(opts, 'hostingViewers', null),
+      $raidViewers: _.get(opts, 'raidViewers', null),
+      $slowModeState: _.get(opts, 'slowModeState', null),
+      $slowModeLength: _.get(opts, 'slowModeLength', null),
+      $subsOnlyChatState: _.get(opts, 'subsOnlyChatState', null),
+    }
+
+    const run = await safeEval(toEval, context)
+    return run
   }
   async load () {
     this.events = await global.db.select(`*`).from('systems.events')
