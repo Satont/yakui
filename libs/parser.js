1b5a45a3717ba36e301cf3f38d880744cf2867ae
Satont <satontworldwide@gmail.com>
2019-09-06 12:00:53 +0300

    upd

--- libs/parser.js
+++ libs/parser.js
@@ -8,22 +8,24 @@ module.exports = (userstate, message) => {
       for (let command of system.commands) {
         let msg = message.replace('!', '').trim()
         if (!msg.startsWith(command.name)) continue // skip command if name not found
-
-        if (typeof command.cooldown === 'undefined' || typeof command.cooldownper === 'undefined') {
+        if (typeof command.cooldown === 'undefined' || typeof command.cooldownfor === 'undefined') {
+          console.log(`typeof command.cooldown === 'undefined' || typeof command.cooldownfor === 'undefined'`)
           userstate['message-type'] = 'chat'
         }
         else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'stop') {
           return global.log.info(`COMMAND ${find.name.toUpperCase()} ON COOLDOWN AND HAS NO EXECUTE MODEL`)
         }
         else if (cooldowns.some(o => o.id === command.id) && (userstate.mod || userstate.subscriber)) {
+          console.log(`cooldowns.some(o => o.id === command.id) && (userstate.mod || userstate.subscriber)`)
           userstate['message-type'] = 'chat'
-        } else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'notstop' && command.cooldowper !== 'user') {
+        } else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'notstop' && command.cooldownfor !== 'user') {
+          console.log(`cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'notstop' && command.cooldownfor !== 'user'`)
           userstate['message-type'] = 'whisper'
-        } else if (command.cooldownper === 'user') {
-          cooldowns.push({ id: command.id, type: 'user' })
-          break; /// we don't want to execute command if command cooldown type is for user
+        } else if (cooldowns.some(o => o.id === command.id) && cooldowns.some(o => o.user === userstate.username) && command.cooldownfor === 'user') {
+          break;
+          
         }
-        else cooldowns.push({ id: command.id, type: 'global' })
+        cooldowns.push({ id: command.id, type: command.cooldownfor, user: userstate.username })
 
         command['fnc'].apply(system, [userstate, msg.replace(command.name, '').trim(), command.response || null])
         setTimeout(() => _.remove(cooldowns, o => o === command.id), command.cooldown * 1000)
