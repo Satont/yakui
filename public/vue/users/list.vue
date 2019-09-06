d49296c6ba6bee24cda21056c38fced45579d6c9
Satont <satontworldwide@gmail.com>
2019-09-06 03:34:28 +0300

    some updates

--- public/vue/users/list.vue
+++ public/vue/users/list.vue
@@ -7,6 +7,8 @@
         {{ data.index + 1 }}
   </template>
 </b-table>
+
+<b-pagination v-model="currentPage" :total-rows="totalRows" :per-page="perPage" align="center" v-on:input="changePage"></b-pagination>
 </div>
 </template>
 
@@ -43,7 +45,8 @@ export default {
     sort: function(o) {
       this.sortDesc = o.sortDesc
       this.sortyBy = o.sortBy
-      this.$socket.emit('users.get', { sortBy: o.sortBy, sortDesc: o.sortDesc, page: o.currentPage, perPage: o.perPage }, (err, data) => {
+      this.currentPage = 1
+      this.$socket.emit('users.get', { sortBy: o.sortBy, sortDesc: o.sortDesc, page: 1, perPage: this.perPage }, (err, data) => {
         this.users = data
       })
     },
@@ -55,3 +58,9 @@ export default {
   }
 }
 </script>
+
+<style>
+#pills-tab li {
+  cursor: pointer;
+}
+</style>
