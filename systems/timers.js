14f0b439636fb8d845da7406985db19aa00e7653
Satont <satontworldwide@gmail.com>
2019-09-16 17:49:48 +0300

    fix(timers): remove commented check

--- systems/timers.js
+++ systems/timers.js
@@ -25,7 +25,7 @@ class Timers {
     else this.timeout = setTimeout(() => this.check(), 10000)
     for (const timer of timers) {
       if ((new Date().getTime() - timer.triggertimestamp) > (timer.interval * 1000)) {
-        //if (!global.tmi.uptime) return
+        if (!global.tmi.uptime) return
         const response = await variables.prepareMessage(timer.responses[timer.last], { 'display-name': process.env.TWITCH_BOTUSERNAME })
         await say(response)
         await global.db('systems.timers').where('name', timer.name).update({ last: ++timer.last % timer.responses.length, triggertimestamp: new Date().getTime() })
