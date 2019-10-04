54a87d2eea0568ffc07f868cf0daee2bf7ed12b8
Satont <satontworldwide@gmail.com>
2019-10-04 23:08:55 +0300

    feat(parser): use system for loop, instead of command names loop

--- libs/parser.js
+++ libs/parser.js
@@ -5,50 +5,48 @@ module.exports = (userstate, message) => {
   const isCommand = message.startsWith('!') ? true : false
   for (let [, system] of Object.entries(global.systems)) {
     if (typeof system.commands !== 'undefined' && isCommand) {
-      for (let command of system.commands) {
-        let msg = message.replace('!', '').trim()
+      let msg = message.replace('!', '').trim()
+      let msgArray = msg.toLowerCase().split(' ')
+      let command = false
 
-        let names_of_command = command.aliases ? _.clone(command.aliases) : []
-        names_of_command.push(command.name)
-
-        let msgArray = msg.toLowerCase().split(' ')
-        let finded = false
-        for (let i = 0, len = msg.split(' ').length; i < len; i++) {
-          const find = names_of_command.find(o => o === msgArray.join(' '))
-          if (!find) msgArray.pop()
-          else finded = true
-        }
-
-        if (!finded) continue;
-
-        if (typeof command.cooldown === 'undefined' || typeof command.cooldownfor === 'undefined') {
-          userstate['message-type'] = 'chat'
-        }
-        else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'stop' && command.cooldownfor === 'global') {
-          return global.log.info(`COMMAND ${command.name.toUpperCase()} ON COOLDOWN AND HAS NO EXECUTE MODEL`)
-        }
-        else if (cooldowns.some(o => o.id === command.id && o.user === userstate.username) && command.cooldowntype === 'stop' && command.cooldownfor ==='user') {
-          return global.log.info(`COMMAND ${command.name.toUpperCase()} ON COOLDOWN FOR USER ${userstate.username} AND HAS NO EXECUTE MODEL`)
-        }
-        else if (cooldowns.some(o => o.id === command.id) && (userstate.mod || userstate.subscriber)) {
-          userstate['message-type'] = 'chat'
-        } else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'notstop' && command.cooldownfor !== 'user') {
-          userstate['message-type'] = 'whisper'
-        } else if (cooldowns.some(o => o.id === command.id && o.user === userstate.username) && command.cooldownfor === 'user') {
-          break;
-        }
-        cooldowns.push({ id: command.id, type: command.cooldownfor, user: userstate.username })
-
-        for (const item of names_of_command) {
-          if (new RegExp('^' + item, 'u').test(msg)) {
-            msg = msg.replace(item, '').trim()
-          }
-        }
-
-        command['fnc'].apply(system, [userstate, msg.trim(), command.response || null])
-        setTimeout(() => _.remove(cooldowns, o => o.id === command.id), command.cooldown * 1000)
-        //break; // stop loop if command was found and axecuted
+      for (let i = 0, len = msg.split(' ').length; i < len; i++) {
+        command = system.commands.find(o => o.name === msgArray.join(' ') || (o.aliases ? o.aliases.includes(msgArray.join(' ')) : false))
+        if (!command) msgArray.pop()
+        else command = command
       }
+
+      if (!command) continue;
+
+      let command_names = command.aliases || []
+      command_names.push(command.name)
+
+      if (typeof command.cooldown === 'undefined' || typeof command.cooldownfor === 'undefined') {
+        userstate['message-type'] = 'chat'
+      }
+      else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'stop' && command.cooldownfor === 'global') {
+        return global.log.info(`COMMAND ${command.name.toUpperCase()} ON COOLDOWN AND HAS NO EXECUTE MODEL`)
+      }
+      else if (cooldowns.some(o => o.id === command.id && o.user === userstate.username) && command.cooldowntype === 'stop' && command.cooldownfor ==='user') {
+        return global.log.info(`COMMAND ${command.name.toUpperCase()} ON COOLDOWN FOR USER ${userstate.username} AND HAS NO EXECUTE MODEL`)
+      }
+      else if (cooldowns.some(o => o.id === command.id) && (userstate.mod || userstate.subscriber)) {
+        userstate['message-type'] = 'chat'
+      } else if (cooldowns.some(o => o.id === command.id) && command.cooldowntype === 'notstop' && command.cooldownfor !== 'user') {
+        userstate['message-type'] = 'whisper'
+      } else if (cooldowns.some(o => o.id === command.id && o.user === userstate.username) && command.cooldownfor === 'user') {
+        break;
+      }
+      
+      cooldowns.push({ id: command.id, type: command.cooldownfor, user: userstate.username })
+
+      for (const item of command_names) {
+        if (new RegExp('^' + item, 'u').test(msg)) {
+          msg = msg.replace(item, '').trim()
+        }
+      }
+
+      command['fnc'].apply(system, [userstate, msg.trim(), command.response || null])
+      setTimeout(() => _.remove(cooldowns, o => o.id === command.id), command.cooldown * 1000)
     } else {
       if (typeof system.parsers === 'undefined') continue
       for (let parser of system.parsers) {
