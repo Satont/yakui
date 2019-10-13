e56612e0eccadce1f86f8e6e8c9f6c1e3e4185af
Satont <satontworldwide@gmail.com>
2019-10-13 21:03:56 +0300

    locales for events is done

--- public/vue/single/events.vue
+++ public/vue/single/events.vue
@@ -21,20 +21,24 @@
   <div class="jumbotron jumbotron-fluid">
     <div class="container">
       <h1 class="display-4">{{ show }} event</h1>
-      <p class="lead">{{ this.$data[show].description }}</p>
+      <p class="lead">
+      {{ translate(`ui.events.${show}.description`) }}
+      </p>
     </div>
   </div>
 
   <table class="table table-dark table-sm">
     <thead>
       <tr align="center">
-        <th colspan="2">Avalible variables for {{ show }} event</th>
+        <th colspan="2">{{ translate('ui.events.avaliableVariables') }}</th>
       </tr>
     </thead>
     <tbody>
       <tr v-for="(variable, index) in this.$data[show].variables" :key="index">
         <td>{{ variable.name }}</td>
-        <td>{{ variable.description }}</td>
+        <td>
+          {{ translate(`ui.events.${show}.variables.${variable.name.replace('$', '')}`)}}
+        </td>
       </tr>
     </tbody>
   </table>
@@ -42,21 +46,32 @@
   <div class="card card-bg" :key="index" v-for="(operation, index) in this.$data[show].operations" style="margin-bottom:15px;">
     <div class="card-body">
       <select class="custom-select" v-model="operation.key" style="margin-bottom:15px;">
-        <option value="sendMessage">Send Chat Message</option>
+        <option value="sendMessage">
+          {{ translate('ui.events.operations.sendChatMessage.label') }}
+        </option>
       </select>
-      <center><label>Filter of operation (javascript)</label></center>
-      <input type="text" class="form-control" v-model="operation.filter" placeholder="$username === 'sadisnamenya'">
+      <center><label>
+        {{ translate('ui.events.operations.sendChatMessage.filterLabel') }}
+      </label></center>
+      <input type="text" class="form-control" v-model="operation.filter" placeholder="$username === 'moobot'">
       <br>
-      <center><label v-if="operation.key === 'sendMessage'">Message for sending</label></center>
-      <input type="text" class="form-control" v-if="operation.key === 'sendMessage'" v-model="operation.message" placeholder="Message for sending">
+      <center><label v-if="operation.key === 'sendMessage'">
+        {{ translate('ui.events.operations.sendChatMessage.messageLabel') }}
+      </label></center>
+      <input type="text" class="form-control" v-if="operation.key === 'sendMessage'" v-model="operation.message">
     </div>
     <div class="card-footer text-muted">
-      <button type="button" class="btn btn-block btn-danger btn-sm" @click="deleteOperation(index)">Delete</button>
+      <button type="button" class="btn btn-block btn-danger btn-sm" @click="deleteOperation(index)">
+        {{ translate('ui.delete') }}
+      </button>
     </div>
   </div>
   <br>
-  <button type="button" class="btn btn-block btn-success" @click="save()" style="margin-bottom:5px;">Save</button>
-  <button type="button" class="btn btn-block btn-success" @click="addOperation()">Add new opperation</button>
+  <button type="button" class="btn btn-block btn-success" @click="save()" style="margin-bottom:5px;">
+    {{ translate('ui.save') }}
+  </button>
+  <button type="button" class="btn btn-block btn-success" @click="addOperation()">
+    {{ translate('ui.events.operations.add') }}</button>
 </div>
 </template>
 
@@ -69,122 +84,107 @@ export default {
       show: 'tip',
       tip: {
         variables: [
-          { name: '$username', description: 'Username of user who donated' },
-          { name: '$amount', description: 'How much donated' },
-          { name: '$currency', description: 'Currency of donate' },
-          { name: '$message', description: 'Message of donation' }
+          { name: '$username' },
+          { name: '$amount' },
+          { name: '$currency' },
+          { name: '$message' }
         ],
-        description: 'Triggering when you get some donation',
         operations: []
       },
       bits: {
         variables: [
-          { name: '$username', description: 'Username of user who donated' },
-          { name: '$amount', description: 'How much donated' },
-          { name: '$message', description: 'Message of donation' }
+          { name: '$username' },
+          { name: '$amount' },
+          { name: '$message' }
         ],
-        description: 'Triggering when you get some cheers',
         operations: []
       },
       sub: {
         variables: [
-          { name: '$username', description: 'Username of user who donated' },
-          { name: '$subTier', description: 'Tier of sub. Twitch prime/1/2/3/' },
-          { name: '$message', description: 'Message of sub' }
+          { name: '$username' },
+          { name: '$subTier' },
+          { name: '$message' }
         ],
-        description: 'Triggering when you get new subscriber',
         operations: []
       },
       resub: {
          variables: [
-          { name: '$username', description: 'Username of user who donated' },
-          { name: '$subTier', description: 'Tier of sub. Twitch prime/1/2/3/' },
-          { name: '$subStreak', description: 'Cumulative streak of user months' },
-          { name: '$message', description: 'Message of resub' }
+          { name: '$username' },
+          { name: '$subTier' },
+          { name: '$subStreak' },
+          { name: '$message' }
         ],
-        description: 'Triggering when some user did resubscribtion',
         operations: []
       },
       subGift: {
         variables: [
-          { name: '$username', description: 'Username of user who donated' },
-          { name: '$subTier', description: 'Tier of sub. Twitch prime/1/2/3/' },
-          { name: '$subStreak', description: 'Cumulative streak of user months' },
-          { name: '$subGiftRecipient', description: 'Username of recipient user' },
-          { name: '$subGifterCount', description: 'Gifted subs count by $username' }
+          { name: '$username' },
+          { name: '$subTier' },
+          { name: '$subStreak' },
+          { name: '$subGiftRecipient' },
+          { name: '$subGifterCount' }
         ],
-        description: 'Triggering when some user gift subscribtion to another user',
         operations: []
       },
       message: {
-        description: 'Triggering when some chat message appears',
         variables: [
-          { name: '$username', description: 'Username of user who sent message' },
+          { name: '$username' },
         ],
         operations: []
       },
       chatClear: {
-        description: 'Triggering when chat was cleared',
         operations: []
       },
       userJoin: {
         variables: [
-          { name: '$username', description: 'Username who joined chat' }
+          { name: '$username' }
         ],
-        description: 'Triggering when some user joined channel',
         operations: []
       },
       userPart: {
         variables: [
-          { name: '$username', description: 'Username who parted chat' }
+          { name: '$username' }
         ],
-        description: 'Triggering when some user parted channel',
         operations: []
       },
       emoteOnly: {
         variables: [
-          { name: '$emoteOnlyState', description: 'Showing does emoteonly enabled or disabled' }
+          { name: '$emoteOnlyState' }
         ],
-        description: 'Triggering when emote only was enabled/disabled on channel',
         operations: []
       },
       hosted: {
         variables: [
-          { name: '$username', description: 'Username who started host' },
-          { name: '$hostedViewers', description: 'Viewers of host' }
+          { name: '$username' },
+          { name: '$hostedViewers' }
         ],
-        description: 'Triggering when you got hosted',
         operations: []
       },
       hosting: {
         variables: [
-          { name: '$username', description: 'Username of target' },
-          { name: '$hostingViewers', description: 'Viewers of host' }
+          { name: '$username' },
+          { name: '$hostingViewers' }
         ],
-        description: 'Triggering when yuo starting host',
         operations: []
       },
       raided: {
         variables: [
-          { name: '$username', description: 'Username raider' },
-          { name: '$raidViewers', description: 'Viewers of raid' }
+          { name: '$username' },
+          { name: '$raidViewers' }
         ],
-        description: 'Triggering when someone start to raid you',
         operations: []
       },
       slowMode: {
         variables: [
-          { name: '$slowModeState', description: 'State of slow mode (enabled/disabled)' },
-          { name: '$slowModeLength', description: 'Length of slow mode' }
+          { name: '$slowModeState' },
+          { name: '$slowModeLength' }
         ],
-        description: 'Triggering when slow mode enabled/disabled in chat',
         operations: []
       },
       subsOnlyChat: {
         variables: [
-          { name: '$subsOnlyChatState', description: 'State of subscribers mode (enabled/disabled)' },
+          { name: '$subsOnlyChatState',  },
         ],
-        description: 'Triggering when subscribers only chat enabled',
         operations: []
       }
     };
