3b79c38d00053efa6c075e14c68ea5660823f7f6
Satont <42675886+Satont@users.noreply.github.com>
2020-04-01 05:21:39 +0300

    fix(db): do not use ssl connection

--- libs/db.js
+++ libs/db.js
@@ -6,7 +6,7 @@ const knex = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    ssl: process.env.NODE_ENV === 'production' ? true : false
+    ssl: false
   },
   // debug: true,
   pool: {
