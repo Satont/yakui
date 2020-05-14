074cb2ac77a94d1cdf7390c2cf61c1ecd5c04cb5
Satont <42675886+Satont@users.noreply.github.com>
2020-05-14 18:50:04 +0300

    Update db.js

--- libs/db.js
+++ libs/db.js
@@ -1,17 +1,21 @@
+const connection = process.env.DATABASE_URL || process.env.DATABASE_URL !== '' 
+  ? process.env.DATABASE_URL
+  : {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: false
+   }
+
 const knex = require('knex')({
   client: 'pg',
-  connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: false
-  },
+  connection,
   // debug: true,
   pool: {
     min: 0,
-    max: 10,
+    max: 5,
     afterCreate: function (conn, done) {
       console.log('Pool created')
       knex.connected = true
