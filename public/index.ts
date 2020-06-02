cabb00b2afba7958a4c2b2fcf84191328632c711
Satont <satontworldwide@gmail.com>
2020-06-03 01:47:24 +0300

    update

--- public/index.ts
+++ public/index.ts
@@ -6,29 +6,28 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './css/main.css'
 
-function lazyLoad(path){
-  return() => import(`./vue/${path}`)
-}
+
+Vue.prototype.lazyLoad = (path: string) => import(`./vue/${path}`)
 
 Vue.use(VueRouter)
 Vue.use(VueClipboard)
 Vue.use(BootstrapVue)
-Vue.component('side-bar', lazyLoad('components/sidebar.vue'))
-Vue.component('nav-bar', lazyLoad('components/navbar.vue'))
+Vue.component('side-bar', () => import('./vue/components/sidebar.vue'))
+Vue.component('nav-bar', () => import('./vue/components/navbar.vue'))
 
 const router = new VueRouter({
   routes: [
-    { path: '/', component: lazyLoad('index.vue'), alias: '/home' },
-    { path: '/settings', component: lazyLoad('pages/settings/general.vue') },
+    { path: '/', component: () => import('./vue/index.vue'), alias: '/home' },
+    { path: '/settings', component: () => import('./vue/pages/settings/index.vue') },
 
-    { path: '/commands', name: 'CommandsManagerList', component: lazyLoad('pages/commands/list.vue') },
-    { path: '/commands/edit/:id?', name: 'CommandsManagerEdit', component: lazyLoad('pages/commands/edit.vue') },
+    { path: '/commands', name: 'CommandsManagerList', component: () => import('./vue/pages/commands/list.vue') },
+    { path: '/commands/edit/:id?', name: 'CommandsManagerEdit', component: () => import('./vue/pages/commands/edit.vue') },
 
-    { path: '/timers', name: 'TimersManagerList', component: lazyLoad('pages/timers/list.vue') },
-    { path: '/timers/edit/:id?', name: 'TimersManagerEdit', component: lazyLoad('pages/timers/edit.vue') },
+    { path: '/timers', name: 'TimersManagerList', component: () => import('./vue/pages/timers/list.vue') },
+    { path: '/timers/edit/:id?', name: 'TimersManagerEdit', component: () => import('./vue/pages/timers/edit.vue') },
 
-    { path: '/users', name: 'UsersManagerList', component: lazyLoad('pages/users/list.vue') },
-    { path: '/users/edit/:id?', name: 'UsersManagerEdit', component: lazyLoad('pages/users/edit.vue') },
+    { path: '/users', name: 'UsersManagerList', component: () => import('./vue/pages/users/list.vue') },
+    { path: '/users/edit/:id?', name: 'UsersManagerEdit', component: () => import('./vue/pages/users/edit.vue') },
   ],
 })
 
---
+++ public/vue/pages/settings/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <b-tabs content-class="mt-3" align="center">
+    <b-tab title="OAuth" active><OAuth /></b-tab>
+  </b-tabs>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
+import OAuth from './oauth.vue'
+
+@Component({
+  components: {
+    OAuth
+  }
+})
+export default class Index extends Vue {
+
+}
+</script>
