3856671aa9393ccd35b51150b12719e4be496284
Satont <satontworldwide@gmail.com>
2020-06-03 02:21:34 +0300

    update

--- src/systems/users.ts
+++ src/systems/users.ts
@@ -1,7 +1,10 @@
 import { System } from '../typings'
 import TwitchPrivateMessage from 'twitch-chat-client/lib/StandardCommands/TwitchPrivateMessage'
 import User from '../models/User'
+import tmi from '../libs/tmi'
 import { literal } from 'sequelize'
+import UserTips from '../models/UserTips'
+import UserBits from '../models/UserBits'
 
 export default new class Users implements System {
   parsers = [
@@ -19,5 +22,27 @@ export default new class Users implements System {
     if (!created) {
       user.update({ username, messages: literal('messages + 1') })
     }
+  }
+
+  async getUserStats({ id, username }: { id?: string, username?: string }): Promise<User> {
+    if (!id && !username) throw new Error('Id or username should be used.')
+
+    if (!id) {
+      const byName = await tmi?.clients?.bot?.helix.users.getUserByName(username)
+      id = byName.id
+    }
+
+    let user = await User.findOne({ 
+      where: { id },
+      include: [UserTips, UserBits],
+      attributes: { include: ['totalTips', 'totalTips' ]},
+    })
+    
+    if (!user) user = await User.create({
+      id,
+      username
+    }, { include: [UserTips, UserBits] })
+
+    return user
   }
 }
--- src/systems/variables.ts
+++ src/systems/variables.ts
@@ -1,7 +1,9 @@
 import TwitchPrivateMessage from 'twitch-chat-client/lib/StandardCommands/TwitchPrivateMessage'
-import tmi from '../libs/tmi'
-import humanizeDuration from 'humanize-duration'
+import { random } from 'lodash'
+
 import twitch from '../libs/twitch'
+import includesOneOf from '../commons/includesOneOf'
+import users from './users'
 
 export default new class Variables {
   async parseMessage(message: string, raw?: TwitchPrivateMessage) {
@@ -10,11 +12,21 @@ export default new class Variables {
     result = result
       .replace(/\$sender/gimu, '@' + raw?.userInfo.userName)
       .replace(/\$followage/gimu, await twitch.getFollowAge(raw?.userInfo.userId))
-      .replace(/\$viewers/gimu, String(twitch.streamMetaData?.viewers ?? 0))
-      .replace(/\$views/gimu, String(twitch.channelMetaData?.views ?? 0))
-      .replace(/\$game/gimu, twitch.streamMetaData?.game)
-      .replace(/\$title/gimu, twitch.streamMetaData?.title)
-      .replace(/\$uptime/gimu, twitch.getUptime())
+      .replace(/\$stream\.viewers/gimu, String(twitch.streamMetaData?.viewers ?? 0))
+      .replace(/\$channel\.views/gimu, String(twitch.channelMetaData?.views ?? 0))
+      .replace(/\$channel\.game/gimu, twitch.streamMetaData?.game)
+      .replace(/\$channel\.title/gimu, twitch.streamMetaData?.title)
+      .replace(/\$stream\.uptime/gimu, twitch.getUptime())
+      .replace(/\$random\.(\d)-(\d)/gimu, (match, first, second) => String(random(first, second)))
+
+    if (includesOneOf(result, ['user.messages', 'user.tips', 'user.bits'])) {
+      const user = await users.getUserStats({ id: raw?.userInfo.userId })
+
+      result = result
+        .replace(/\$user\.messages/gimu, String(user.messages))
+        .replace(/\$user\.tips/gimu, String(user.totalTips))
+        .replace(/\$user\.bits/gimu, String(user.totalBits))
+    }
 
     return result
   }
