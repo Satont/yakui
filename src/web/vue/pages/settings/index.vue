1ba2e15d8cd237d14446e132664683f8180e7fc8
Satont <satontworldwide@gmail.com>
2020-06-03 20:30:48 +0300

    upd

--- src/web/vue/pages/settings/index.vue
+++ src/web/vue/pages/settings/index.vue
@@ -1,6 +1,7 @@
 <template>
   <b-tabs content-class="mt-3" align="center">
     <b-tab title="OAuth" active><OAuth /></b-tab>
+    <b-tab title="Moderation"><Moderation /></b-tab>
   </b-tabs>
 </template>
 
@@ -8,10 +9,12 @@
 import { Vue, Component } from 'vue-property-decorator'
 import axios from 'axios'
 import OAuth from './oauth.vue'
+import Moderation from './moderation.vue'
 
 @Component({
   components: {
-    OAuth
+    OAuth,
+    Moderation
   }
 })
 export default class Index extends Vue {
---
+++ src/web/vue/pages/settings/moderation.vue
@@ -0,0 +1,204 @@
+<template>
+  <div>
+    <b-form @submit.prevent="save">
+      <b-button class="btn-block" variant="success" v-if="settings.enabled" @click.prevent="settings.settings = !settings.enabled">Enabled</b-button>
+      <b-button class="btn-block" variant="warning" v-if="!settings.enabled" @click.prevent="settings.settings = !settings.enabled">Disabled</b-button>
+      <b-button class="btn-block" type="submit" variant="primary">Save</b-button>
+
+      <div class="row cards mt-3">
+
+        <div class="col-md-6">
+          <div class="card bg-dark">
+            <div class="card-header">Links</div>
+            <div class="card-body">
+              <div class="btn-group btn-group-sm d-flex mb-3">
+                <b-btn size="sm" class="w-100" v-bind:class="{ 'btn-success': settings.links.enabled, 'btn-danger': !settings.links.enabled }" @click="settings.links.enabled = !settings.links.enabled">
+                  <span v-show="settings.links.enabled">Enabled</span>
+                  <span v-show="!settings.links.enabled">Disabled</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.links.subscribers, 'btn-danger': !settings.links.subscribers }" @click="settings.links.subscribers = !settings.links.subscribers">
+                  <span>Moderate subscribers</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.links.vips, 'btn-danger': !settings.links.vips }" @click="settings.links.vips = !settings.links.vips">
+                  <span>Moderate vips</span>
+                </b-btn>
+              </div>
+
+              <b-input-group size="sm" prepend="Timeout time" append="message">
+                <b-form-input type="number" v-model.number="settings.links.timeout.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.links.timeout.message"></b-form-input>
+              </b-input-group>
+
+              <b-input-group size="sm" class="mt-1" prepend="Warning time" append="message">
+                <b-form-input type="number" v-model.number="settings.links.warning.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.links.warning.message"></b-form-input>
+              </b-input-group>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="card bg-dark">
+            <div class="card-header">Symbols</div>
+            <div class="card-body">
+              <div class="btn-group btn-group-sm d-flex mb-3">
+                <b-btn size="sm" class="w-100" v-bind:class="{ 'btn-success': settings.symbols.enabled, 'btn-danger': !settings.symbols.enabled }" @click="settings.symbols.enabled = !settings.symbols.enabled">
+                  <span v-show="settings.symbols.enabled">Enabled</span>
+                  <span v-show="!settings.symbols.enabled">Disabled</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.symbols.subscribers, 'btn-danger': !settings.symbols.subscribers }" @click="settings.symbols.subscribers = !settings.symbols.subscribers">
+                  <span>Moderate subscribers</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.symbols.vips, 'btn-danger': !settings.symbols.vips }" @click="settings.symbols.vips = !settings.symbols.vips">
+                  <span>Moderate vips</span>
+                </b-btn>
+              </div>
+
+              <b-input-group size="sm" prepend="Trigger length" append="max percentage">
+                <b-form-input type="number" v-model.number="settings.symbols.trigger['length']"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.symbols.trigger.percent"></b-form-input>
+              </b-input-group>
+
+              <b-input-group size="sm" class="mt-1" prepend="Timeout time" append="message">
+                <b-form-input type="number" v-model.number="settings.symbols.timeout.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.symbols.timeout.message"></b-form-input>
+              </b-input-group>
+
+              <b-input-group size="sm" class="mt-1" prepend="Warning time" append="message">
+                <b-form-input type="number" v-model.number="settings.symbols.warning.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.symbols.warning.message"></b-form-input>
+              </b-input-group>
+
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="card bg-dark">
+            <div class="card-header">Long message</div>
+            <div class="card-body">
+              <div class="btn-group btn-group-sm d-flex mb-3">
+                <b-btn size="sm" class="w-100" v-bind:class="{ 'btn-success': settings.longMessage.enabled, 'btn-danger': !settings.longMessage.enabled }" @click="settings.longMessage.enabled = !settings.longMessage.enabled">
+                  <span v-show="settings.longMessage.enabled">Enabled</span>
+                  <span v-show="!settings.longMessage.enabled">Disabled</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.longMessage.subscribers, 'btn-danger': !settings.longMessage.subscribers }" @click="settings.longMessage.subscribers = !settings.longMessage.subscribers">
+                  <span>Moderate subscribers</span>
+                </b-btn>
+                <b-btn class="w-100" v-bind:class="{ 'btn-success': settings.longMessage.vips, 'btn-danger': !settings.longMessage.vips }" @click="settings.longMessage.vips = !settings.longMessage.vips">
+                  <span>Moderate vips</span>
+                </b-btn>
+              </div>
+
+              <b-input-group size="sm" prepend="Trigger length" >
+                <b-form-input type="number" v-model.number="settings.longMessage.trigger['length']"></b-form-input>
+              </b-input-group>
+
+              <b-input-group size="sm" class="mt-1" prepend="Timeout time" append="message">
+                <b-form-input type="number" v-model.number="settings.longMessage.timeout.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.longMessage.timeout.message"></b-form-input>
+              </b-input-group>
+
+              <b-input-group size="sm" class="mt-1" prepend="Warning time" append="message">
+                <b-form-input type="number" v-model.number="settings.longMessage.warning.time"></b-form-input>
+                <b-form-input type="text" v-model.trim="settings.longMessage.warning.message"></b-form-input>
+              </b-input-group>
+
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </b-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import axios from 'axios'
+
+@Component
+export default class ModerationSettings extends Vue {
+  settings = {
+    space: 'moderation',
+    enabled: false,
+    links: {
+      enabled: false,
+      vips: false,
+      subscribers: false,
+      timeout: {
+        time: 600,
+        message: 'links disallowed'
+      },
+      warning: {
+        time: 10,
+        message: 'links disallowed [warn]'
+      }
+    },
+    symbols: {
+      enabled: false,
+      vips: false,
+      subscribers: false,
+      timeout: {
+        time: 600,
+        message: 'to much symbols'
+      },
+      warning: {
+        time: 10,
+        message: 'to much symbols [warn]'
+      },
+      trigger: {
+        length: 15,
+        percent: 50
+      },
+    },
+    longMessage: {
+      enabled: false,
+      vips: false,
+      subscribers: false,
+      timeout: {
+        time: 600,
+        message: 'so long message'
+      },
+      warning: {
+        time: 10,
+        message: 'so long message [warn]'
+      },
+      trigger: {
+        length: 300,
+      },
+    }
+  }
+
+  async save() {
+    const space = this.settings.space
+    const data = Object.entries(this.settings)
+      .filter(v => v[0] !== 'space')
+      .map((item) => ({ space, name: item[0], value: item[1] }))
+
+    await axios.post('/api/v1/settings', data)
+  }
+
+  async created() {
+    const { data } = await axios.get('/api/v1/settings?space=' + this.settings.space)
+    for (const item of data) {
+      this.settings[item.name] = item.value
+    }
+  }
+}
+</script>
+
+<style scoped>
+.cards > div > div.card {
+  height: calc(100% - 15px);
+  margin-bottom: 15px;
+}
+.card-span {
+  display: block !important;
+  white-space: normal !important;
+}
+.card-body > div.btn-group {
+  margin-bottom: 5px;
+}
+</style>
