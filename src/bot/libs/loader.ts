26483d9abfe3415bac92d849685093e8421d16a7
Satont <satontworldwide@gmail.com>
2020-06-03 16:58:19 +0300

    upd loader

--- src/bot/libs/loader.ts
+++ src/bot/libs/loader.ts
@@ -3,10 +3,20 @@ import getFiles from '../commons/getFiles'
 
 const loader = async () => {
   for await (const file of getFiles(resolve(__dirname, '..', 'systems'))) {
+    if (!file.endsWith('.js') && !file.endsWith('.ts')) continue;
     const loadedFile = await import(resolve(__dirname, '..', 'systems', file))
 
     if (typeof loadedFile.default.init !== 'undefined') await loadedFile.default.init()
     console.log(`System ${loadedFile.default.constructor.name.toUpperCase()} loaded`)
+  }
+
+  for await (const file of getFiles(resolve(__dirname, '..', 'customSystems'))) {
+    if (!file.endsWith('.js') && !file.endsWith('.ts')) continue;
+
+    const loadedFile = await import(resolve(__dirname, '..', 'customSystems', file))
+
+    if (typeof loadedFile.default.init !== 'undefined') await loadedFile.default.init()
+    console.log(`Custom System ${loadedFile.default.constructor.name.toUpperCase()} loaded`)
   }
 }
 
