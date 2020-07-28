446deb5c730df241d7f57f7552a57e443b16e72d
Satont <satontworldwide@gmail.com>
2020-07-28 20:09:23 +0300

    update

--- src/panel/vue/index.vue
+++ src/panel/vue/index.vue
@@ -1,17 +1,17 @@
 <template>
 <div>
   <vue-draggable-resizable
-  class-name-active="my-active-class"
-  v-for="element in [{ name: 'chat', id: 0, left: 100, top: 100, width: 560, height: 560 }]"
-  :key="element.id"
-  :x="element.left"
-  :y="element.top"
-  :w="element.width"
-  :h="element.height"
-  :resizable="true"
-  @dragstop="(left, top) => onDrag(element.id, left, top)"
-  @resizestop="(left, top, width, height) => onResize(element.id, left, top, width, height)"
->
+    class-name-active="my-active-class"
+    v-for="element in [{ name: 'chat', id: 0, left: -22, top: -7, width: 560, height: 560 }]"
+    :key="element.id"
+    :x="element.left"
+    :y="element.top"
+    :w="element.width"
+    :h="element.height"
+    :resizable="true"
+    @dragstop="(left, top) => onDrag(element.id, left, top)"
+    @resizestop="(left, top, width, height) => onResize(element.id, left, top, width, height)"
+  >
   <component :is="element.name" />
 </vue-draggable-resizable>
 </div>
--- src/panel/vue/widgets/_interface.vue
+++ src/panel/vue/widgets/_interface.vue
@@ -1,7 +1,7 @@
 <template>
-<b-card style="color:#000" class="h-100" body-class="p-0 h-100">
-  <template v-slot:header class="p-0">
-    <h6 class="mb-0">{{ title | capitalize }}</h6>
+<b-card style="color:#000" class="h-100" body-class="p-0 h-100" header-class="p-1">
+  <template v-slot:header>
+    <span class="title">{{ title | capitalize }}</span> <b-btn variant="danger" size="sm" class="float-right">Delete</b-btn>
   </template>
   <b-card-body class="p-0 h-100">
     <slot />
@@ -26,3 +26,8 @@ export default class Interface extends Vue {
 
 }
 </script>
+<style scoped>
+.title {
+  font-weight: bold;
+}
+</style>
