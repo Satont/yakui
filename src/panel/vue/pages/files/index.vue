3492fc59d624b1d730f1522bb28fb0d41165765d
Satont <satontworldwide@gmail.com>
2020-09-02 07:37:05 +0300

    remove unused import

--- src/panel/vue/pages/files/index.vue
+++ src/panel/vue/pages/files/index.vue
@@ -29,7 +29,7 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
 import { Route } from 'vue-router'
-import socket, { getNameSpace } from '@panel/vue/plugins/socket'
+import { getNameSpace } from '@panel/vue/plugins/socket'
 import _ from 'lodash'
 
 @Component
