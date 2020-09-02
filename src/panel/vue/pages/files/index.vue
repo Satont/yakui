4ed783db94450a0d282b047e94133faa241e5792
Satont <satontworldwide@gmail.com>
2020-09-02 07:36:43 +0300

    update

--- src/panel/vue/pages/files/index.vue
+++ src/panel/vue/pages/files/index.vue
@@ -1,27 +1,27 @@
 <template>
 <div>
   <form method="post" enctype="multipart/form-data" @submit.prevent="submit">
-      <input type="file" id="filesForUpload" ref="filesForUpload" multiple v-on:change="handleFileUploads" />
-      <input type="submit" value="Send" v-bind:disabled="!this.loading" />
+    <input type="file" id="filesForUpload" ref="filesForUpload" multiple v-on:change="handleFileUploads" />
+    <input type="submit" value="Send" v-bind:disabled="!this.loading" />
   </form>
   <br>
   <div class="card-deck mb-3" v-for="(chunk, index) of chunks" :key="'chunk-' + index">
-      <div class="card" v-for="file of chunk" :key="file.id">
-        <div class="card-body border-top p-0 text-right" style="flex: 0 1 auto;">
-          <a v-bind:href="'/api/v1/files/'+ file.id" class="btn btn-outline-dark p-3 border-0 w-100" target="_blank"> {{ file.name || file.id }}</a>
-        </div>
-        <div class="card-body border-top p-0 text-right" style="flex: 1 1 auto;">
-          <img class="w-100" :src="file.data" v-if="file.type.startsWith('image')">
-          <audio controls v-if="file.type.startsWith('audio')">
-            <source v-bind:src="file.data" :type="file.type"/>
-          </audio>
-        </div>
-
-        <div class="card-footer p-0">
-          <b-btn @click="del(file.id)" variant="danger" class="btn-reverse w-100">Delete</b-btn>
-        </div>
+    <div class="card" v-for="file of chunk" :key="file.id">
+      <div class="card-body border-top p-0 text-right" style="flex: 0 1 auto;">
+        <a v-bind:href="'/api/v1/files/'+ file.id" class="btn btn-outline-dark p-3 border-0 w-100" target="_blank">{{ file.name || file.id }}</a>
       </div>
-  <div class="card" v-for="index in (4 - chunk.length)" style="visibility: hidden" :key="'empty-' + index"></div>
+      <div class="card-body border-top p-0 text-right" style="flex: 1 1 auto;">
+        <img class="w-100" :src="file.data" v-if="file.type.startsWith('image')">
+        <audio controls v-if="file.type.startsWith('audio')">
+          <source v-bind:src="file.data" :type="file.type"/>
+        </audio>
+      </div>
+
+       <div class="card-footer p-0">
+        <b-btn @click="del(file.id)" variant="danger" class="btn-reverse w-100">Delete</b-btn>
+      </div>
+    </div>
+    <div class="card" v-for="index in (4 - chunk.length)" style="visibility: hidden" :key="'empty-' + index"></div>
   </div>
 </div>
 </template>
