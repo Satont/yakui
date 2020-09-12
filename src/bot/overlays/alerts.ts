c318feac5075e569892f70c728aee7d325f30331
Satont <satontworldwide@gmail.com>
2020-09-12 16:27:45 +0300

    update

--- src/bot/overlays/alerts.ts
+++ src/bot/overlays/alerts.ts
@@ -3,8 +3,11 @@ import { getNameSpace } from "@bot/libs/socket"
 
 export default new class Alerts implements System {
   socket = getNameSpace('overlays/alerts')
+  clients: SocketIO.Socket[] = []
 
   sockets(client: SocketIO.Socket) {
+    this.clients.push(client)
+    client.on('disconnect', () => this.clients.unshift(client)) // delete this socket from array for fix memory leak
     client.emit('alert', { someObjectData: 'qwe'})
   }
 }
