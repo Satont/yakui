59825b5f1082a5f85f46500145905b9bd7f5bc96
Satont <satontworldwide@gmail.com>
2020-09-12 16:23:49 +0300

    upd

--- src/overlays/vue/alerts.vue
+++ src/overlays/vue/alerts.vue
@@ -11,20 +11,27 @@ import { Vue, Component } from 'vue-property-decorator'
 import Overlay from '@bot/models/Overlay'
 import axios from 'axios'
 import { getNameSpace } from '@panel/vue/plugins/socket'
+import * as Events from 'typings/events'
 
 @Component
 export default class Alerts extends Vue {
   socket = getNameSpace({ name: 'overlays/alerts', opts: { query: { isPublic: true } } })
   playing = false
-  alerts = []
+  alerts: Array<
+    Events.IWebHookModeratorRemove
+    | Events.IWebHookModeratorAdd
+    | Events.IWebHookUserFollow
+    | Events.IWebHookStreamChanged
+    | Events.INewSubscriber
+    | Events.INewResubscriber
+  > = []
 
   mounted() {
     console.log('alerts overlay loaded')
     this.socket.on('alert', async (data) => {
-      console.log(data)
       this.alerts.push(data)
     })
-    //this.setupInterval()
+    this.setupInterval()
   }
 
   setupInterval() {
