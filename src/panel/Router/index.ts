3c92993441bf9a565bc5d79c75fec880a399a539
Satont <satontworldwide@gmail.com>
2020-09-24 17:44:24 +0300

    upd

--- src/panel/Router/index.ts
+++ src/panel/Router/index.ts
@@ -6,6 +6,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      component: { template: '<div></div>' }, // this is needed, because we mount dashboard directly in App.vue
+    },
+    {
       path: '/events',
       component: () => import('../Pages/Events.vue'),
     },
