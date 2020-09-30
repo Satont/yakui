fe81d6252fe7dcf18a0654c0ffc617ab55792069
Satont <satontworldwide@gmail.com>
2020-09-30 09:07:37 +0300

    upd

---
+++ src/typings/overlays.ts
@@ -0,0 +1,21 @@
+import { File } from '@src/bot/entities/File'
+
+export interface IEmitAlert {
+  audio?: {
+    file: File,
+    volume?: number
+  },
+  image?: { 
+    file: File,
+    position: IEmitAlertPosition
+  },
+  text?: { 
+    value: string,
+    position: IEmitAlertPosition,
+  }
+}
+
+export interface IEmitAlertPosition {
+  x: number,
+  y: number,
+}
