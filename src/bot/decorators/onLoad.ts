e1f8c1a89c7fab18daeec689f2a829f55cd51c76
Satont <satontworldwide@gmail.com>
2020-12-24 18:59:24 +0300

    OnLoadedDecorator: call method from instance of class

--- src/bot/decorators/onLoad.ts
+++ src/bot/decorators/onLoad.ts
@@ -1,12 +1,13 @@
-import { loaded } from '../libs/loader'
+import { loaded, loadedSystems } from '../libs/loader'
 
 export function OnLoadedDecorator(): MethodDecorator {
   return (instance: any, methodName: PropertyKey): void => {
     const bootstrap = async () => {
-      if (!loaded) return setTimeout(() => bootstrap(), 1000)
-      await instance[methodName]()
-    }
+      const module = loadedSystems.find(s => s.constructor.name.toLowerCase() === instance.constructor.name.toLowerCase())
+      if (!module || !loaded) return setTimeout(() => bootstrap(), 1000)
 
+      await module[methodName]()
+    }
     bootstrap()
   }
 }
