ddaf8bb12da8be2987b59273ad692081fa45f405
Satont <satontworldwide@gmail.com>
2021-04-30 02:51:25 +0300

    Make default commands enabled by default

--- src/bot/decorators/command.ts
+++ src/bot/decorators/command.ts
@@ -3,7 +3,12 @@ import { System, Command } from 'typings';
 export function command(opts: Command): MethodDecorator {
   return (instance: System, methodName: string): void => {
     if (!instance.commands) instance.commands = [];
-    const data = { ...opts, fnc: methodName };
+    const data = {
+      ...opts,
+      fnc: methodName,
+      enabled: opts.enabled ?? true,
+      visible: opts.visible ?? true,
+    };
 
     instance.commands.push(data);
   };
