3e64bbabf00ff645de3ad79634002a0b82377f4f
Satont <satontworldwide@gmail.com>
2021-05-31 16:16:34 +0300

    Fix crach on greeting get

--- src/bot/panel/routes/api/v1/greetings.ts
+++ src/bot/panel/routes/api/v1/greetings.ts
@@ -19,7 +19,7 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', isAdmin, async (req, res, next) => {
   const greeting = cache.greetings.get(req.params.id);
-  greeting.sound_file = greeting.sound_file.id as any;
+  greeting.sound_file = (greeting.sound_file?.id as any) ?? null;
 
   try {
     res.json(greeting);
@@ -28,87 +28,96 @@ router.get('/:id', isAdmin, async (req, res, next) => {
   }
 });
 
-router.post('/', isAdmin, checkSchema({
-  id: {
-    isNumeric: true,
-    in: ['body'],
-    optional: true,
-  },
-  username: {
-    in: ['body'],
-    optional: true,
-  },
-  userId: {
-    in: ['body'],
-    optional: true,
-  },
-  message: {
-    isString: true,
-    in: ['body'],
-  },
-  enabled: {
-    isBoolean: true,
-    in: ['body'],
-  },
-  sound_file: {
-    isNumeric: true,
-    in: ['body'],
-    optional: {
-      options: {
-        nullable: true,
-        checkFalsy: false,
+router.post(
+  '/',
+  isAdmin,
+  checkSchema({
+    id: {
+      isNumeric: true,
+      in: ['body'],
+      optional: true,
+    },
+    username: {
+      in: ['body'],
+      optional: true,
+    },
+    userId: {
+      in: ['body'],
+      optional: true,
+    },
+    message: {
+      isString: true,
+      in: ['body'],
+    },
+    enabled: {
+      isBoolean: true,
+      in: ['body'],
+    },
+    sound_file: {
+      isNumeric: true,
+      in: ['body'],
+      optional: {
+        options: {
+          nullable: true,
+          checkFalsy: false,
+        },
       },
     },
-  },
-  sound_volume: {
-    isNumeric: true,
-    in: ['body'],
-    optional: {
-      options: {
-        nullable: true,
-        checkFalsy: false,
+    sound_volume: {
+      isNumeric: true,
+      in: ['body'],
+      optional: {
+        options: {
+          nullable: true,
+          checkFalsy: false,
+        },
       },
     },
+  }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      validationResult(req).throw();
+      const body = req.body;
+
+      const repository = RequestContext.getEntityManager().getRepository(Greeting);
+      const greeting = body.id ? await repository.findOne({ id: Number(body.id) }) : repository.create(body);
+
+      repository.assign(greeting, {
+        userId: body.userId ? Number(body.userId) : null,
+        ...body,
+      });
+
+      await repository.persistAndFlush(greeting);
+      await cache.updateGreetings();
+      res.json(greeting);
+    } catch (e) {
+      next(e);
+    }
   },
-}), async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    validationResult(req).throw();
-    const body = req.body;
+);
 
-    const repository = RequestContext.getEntityManager().getRepository(Greeting);
-    const greeting = body.id ? await repository.findOne({ id: Number(body.id) }) : repository.create(body);
+router.delete(
+  '/',
+  isAdmin,
+  checkSchema({
+    id: {
+      isNumeric: true,
+      in: ['body'],
+    },
+  }),
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      validationResult(req).throw();
+      const repository = RequestContext.getEntityManager().getRepository(Greeting);
+      const greeting = await repository.findOne({ id: Number(req.body.id) });
 
-    repository.assign(greeting, {
-      userId: body.userId ? Number(body.userId) : null,
-      ...body,
-    });
-
-    await repository.persistAndFlush(greeting);
-    await cache.updateGreetings();
-    res.json(greeting);
-  } catch (e) {
-    next(e);
-  }
-});
-
-router.delete('/', isAdmin, checkSchema({
-  id: {
-    isNumeric: true,
-    in: ['body'],
+      await repository.removeAndFlush(greeting);
+      await cache.updateGreetings();
+      res.send('Ok');
+    } catch (e) {
+      next(e);
+    }
   },
-}), async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    validationResult(req).throw();
-    const repository = RequestContext.getEntityManager().getRepository(Greeting);
-    const greeting = await repository.findOne({ id: Number(req.body.id) });
-
-    await repository.removeAndFlush(greeting);
-    await cache.updateGreetings();
-    res.send('Ok');
-  } catch (e) {
-    next(e);
-  }
-});
-
+);
 
 export default router;
--- src/panel/vue/pages/greetings/edit.vue
+++ src/panel/vue/pages/greetings/edit.vue
@@ -2,7 +2,13 @@
   <div>
     <b-form v-on:submit.prevent="onSubmit">
       <b-form-group label="Greeting username" label-for="name">
-        <b-form-input id="name" :formatter="(v) => v.toLowerCase()" v-model.trim="greeting.username" type="text" placeholder="Enter username"></b-form-input>
+        <b-form-input
+          id="name"
+          :formatter="(v) => v.toLowerCase()"
+          v-model.trim="greeting.username"
+          type="text"
+          placeholder="Enter username"
+        ></b-form-input>
       </b-form-group>
 
       <b-form-group label="Greeting userId" label-for="userId">
@@ -10,23 +16,25 @@
       </b-form-group>
 
       <b-form-group>
-        <template slot="label" label-for="message">
-           Greeting message <variables-list></variables-list>
-        </template>
+        <template slot="label" label-for="message"> Greeting message <variables-list></variables-list> </template>
         <b-form-input id="message" v-model.trim="greeting.message" required type="text" placeholder="Enter message for user"></b-form-input>
       </b-form-group>
 
-       <b-form-group label="Greeting sound" label-for="sound">
+      <b-form-group label="Greeting sound" label-for="sound">
         <b-form-select id="sound" v-model="greeting.sound_file" :options="selectOptions"></b-form-select>
       </b-form-group>
 
       <div v-if="greeting.sound_file">
-        <label for='pitch'>Sound volume: {{ greeting.sound_volume }}</label>
-        <b-form-input id='pitch' v-model='greeting.sound_volume' type='range' min='1' max='100' step="1"></b-form-input>
+        <label for="pitch">Sound volume: {{ greeting.sound_volume }}</label>
+        <b-form-input id="pitch" v-model="greeting.sound_volume" type="range" min="1" max="100" step="1"></b-form-input>
       </div>
 
-      <b-button class="btn-block" variant="success" v-if="greeting.enabled" v-on:click="greeting.enabled = !greeting.enabled">Enabled</b-button>
-      <b-button class="btn-block" variant="warning" v-if="!greeting.enabled" v-on:click="greeting.enabled = !greeting.enabled">Disabled</b-button>
+      <b-button class="btn-block" variant="success" v-if="greeting.enabled" v-on:click="greeting.enabled = !greeting.enabled"
+        >Enabled</b-button
+      >
+      <b-button class="btn-block" variant="warning" v-if="!greeting.enabled" v-on:click="greeting.enabled = !greeting.enabled"
+        >Disabled</b-button
+      >
 
       <b-button class="btn-block" type="submit" variant="primary">Save</b-button>
       <b-button class="btn-block" @click="del" variant="danger" v-if="greeting.id">Delete</b-button>
@@ -35,60 +43,59 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator'
-import { Route } from 'vue-router'
+import { Vue, Component } from 'vue-property-decorator';
 
 @Component
 export default class GreetingsManagerEdit extends Vue {
   greeting = {
+    id: undefined,
     username: null,
     userId: null,
     enabled: true,
     message: null,
     sound_file: null,
     sound_volume: 50,
-  }
+  };
 
   get audiosList(): any[] {
-    return this.$store.state.filesList?.filter(s => s.type.startsWith('audio'))?.map(file => ({ value: file.id, text: file.name })) || []
+    return (
+      this.$store.state.filesList?.filter((s) => s.type.startsWith('audio'))?.map((file) => ({ value: file.id, text: file.name })) || []
+    );
   }
 
   get selectOptions() {
-    return [
-      { value: null, text: 'No sound' },
-      ...this.audiosList
-    ]
+    return [{ value: null, text: 'No sound' }, ...this.audiosList];
   }
 
   async onSubmit(event) {
-    event.preventDefault()
+    event.preventDefault();
 
     if (!this.greeting.username && !this.greeting.userId) {
-      return alert('Please enter userId or username')
+      return alert('Please enter userId or username');
     }
 
-    await this.$axios.post('/greetings', this.greeting)
-    await this.$router.push({ name: 'GreetingsManagerList' })
-    this.$toast.success('Success')
+    await this.$axios.post('/greetings', this.greeting);
+    await this.$router.push({ name: 'GreetingsManagerList' });
+    this.$toast.success('Success');
   }
 
   async created() {
-    const id = this.$route.params.id as any
+    const id = this.$route.params.id as any;
 
     if (id) {
-      this.greeting = this.$route.params as any
+      this.greeting = this.$route.params as any;
 
-      const { data } = await this.$axios.get('/greetings/' + id)
+      const { data } = await this.$axios.get('/greetings/' + id);
 
-      this.greeting = data
+      this.greeting = data;
     }
   }
 
   async del() {
     await this.$axios.delete('/greetings', {
       data: { id: (this.greeting as any).id },
-    })
-    this.$toast.success('Success')
+    });
+    this.$toast.success('Success');
   }
 }
 </script>
