4df15a9cb992e151ac437ffe9344efbbf54b7645
Satont <satontworldwide@gmail.com>
2021-07-17 07:25:30 +0300

    Fix migration of enums
    [skip-deploy]

--- prisma/migrations/20210703215733_enum_permissions/migration.sql
+++ prisma/migrations/20210703215733_enum_permissions/migration.sql
@@ -1,5 +1,7 @@
 CREATE TYPE "CommandPermission" AS ENUM ('VIEWERS', 'FOLLOWERS', 'VIPS', 'SUBSCRIBERS', 'MODERATORS', 'BROADCASTER');
 
+ALTER TABLE "commands" DROP CONSTRAINT IF EXISTS "commands_permission_check";
+
 UPDATE "commands" SET "permission"=upper("permission");
 
 ALTER TABLE "commands" ALTER COLUMN "permission" DROP DEFAULT;
